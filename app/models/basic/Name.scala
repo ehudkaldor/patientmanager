d29a31154c572224a77d2ef97caf1b6c8aa773a4
ehud <ehud.kaldor@gmail.com>
2015-02-18 22:45:57 -0800

    added  with Timestamps with Datestamps

--- app/models/basic/Name.scala
+++ app/models/basic/Name.scala
@@ -1,9 +1,13 @@
 package models.basic
 
-import com.github.aselab.activerecord.{ActiveRecord, ActiveRecordCompanion}
+import com.github.aselab.activerecord.{ActiveRecord, ActiveRecordCompanion, Timestamps, Datestamps}
 import play.api.libs.json.Json
+import models.Person
 
-case class Name(first: String, middle: Option[String], last: String, val patientId: Option[Long] = None, override val id: Long = 0) extends ActiveRecord
+case class Name(first: String, middle: Option[String], last: String) extends ActiveRecord with Timestamps with Datestamps{
+  lazy val person = hasOne[Person]
+//  val personId: Option[Long] = None
+}
 
 object Name extends ActiveRecordCompanion[Name] {
     implicit val nameFormat = Json.format[Name]
