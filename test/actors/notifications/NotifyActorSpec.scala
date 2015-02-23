eff7a0d2205b08df328e0739ef63d24f6cf79dd8
ehud <ehud.kaldor@gmail.com>
2015-02-23 00:15:32 -0800

    added expectMsgClass as test condition (for ConfirmSubscription)

--- test/actors/notifications/NotifyActorSpec.scala
+++ test/actors/notifications/NotifyActorSpec.scala
@@ -8,6 +8,7 @@ import akka.actor.Props
 import actors.message.dao.NotificationMessage
 import actors.message.dao.Subscribe
 import java.util.UUID
+import actors.message.dao.SubscribeConfirm
 
 class NotifyActorSpec extends Specification with NoTimeConversions {
   sequential // forces all tests to be run sequentially
@@ -15,6 +16,7 @@ class NotifyActorSpec extends Specification with NoTimeConversions {
     "" in new ActorTestkitSpecs2Base {
       within(1 second) {
         system.actorOf(Props[NotifyActor], "NotifyActor") ! NotificationMessage("patients", Subscribe(UUID.randomUUID()))
+        expectMsgClass(classOf[SubscribeConfirm])
       }
     }
   }
