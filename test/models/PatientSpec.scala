cc00db682a273bf55ad2ecb26b00a4e837ffd6d8
ehud <ehud.kaldor@gmail.com>
2015-02-23 00:16:33 -0800

    testing on how to best create a patient with all sub-members

--- test/models/PatientSpec.scala
+++ test/models/PatientSpec.scala
@@ -1,14 +1,11 @@
 package models
 
 import com.github.aselab.activerecord.ActiveRecordSpecification
-//import com.github.aselab.activerecord.dsl._
+import com.github.aselab.activerecord.dsl._
 import org.joda.time.DateTime
-import models.basic.AddressGroup
-import models.basic.Address
-import models.basic.Name
-import models.basic.AddressGroup
+import models.basic.{AddressGroup, Address, Name}
 
-object PersonSpec extends ActiveRecordSpecification {
+object PatientSpec extends ActiveRecordSpecification {
   "Create Person" should {
     "Create transient person with no values" in {
       val dob = DateTime.now
@@ -18,35 +15,43 @@ object PersonSpec extends ActiveRecordSpecification {
     }
   }
   
-  "Create and persist Person" should {
-    "Save a Person with no values" in {
+  "Create and persist Patient" should {
+    "Save a Patient with no values" in {
       val dob = DateTime.now
-      val person = Patient(dob.getMillis).create
-      person must not be null
-      person.dateOfBirth must equalTo(dob.getMillis)
+      val patient = Patient(dob.getMillis).create
+      patient must not be null
+      patient.dateOfBirth must equalTo(dob.getMillis)
       
-      Patient.find(person.id) must not be(None)
-      Patient.find(person.id).get must equalTo(person)
+      Patient.find(patient.id) must not be(None)
+      Patient.find(patient.id).get must equalTo(patient)
     }
     
-    "Create and persist Person with Name and Address" in {
+    "Create and persist Patient with Name and Address" in {
       val dob = DateTime.now
       val patient = Patient(dob.getMillis).create
       val name = Name("first", None, "last").create
       val address = Address("street1", None, None, "city", "state", "zip", "country").create
       val addressGroup = AddressGroup().create
+      val id = patient.id
       
-      addressGroup.mainAddress := address
+//      addressGroup.mainAddress := address
+//      addressGroup.addresses << address
       
+      patient.name := name
+
+//      patient.addressGroup := addressGroup      
+//      addressGroup.mainAddress := address
       
-//      patient.name := name
-//      patient.addressGroup := addressGroup
+      Patient.find(id) must not be(None)
       
-      Patient.find(patient.id) must not be(None)
-      Patient.find(patient.id).get.name.build must not be(None)
-//      Patient.find(patient.id).get.name.toOption.get must equalTo(name)
-//
-//      Patient.find(patient.id).get.addressGroup.toOption must not be(None)
+      println(Patient.find(id).get.name.toOption)
+
+      Patient.find(id).get.name must not be(None)
+      Patient.find(id).get.name.toOption.get must equalTo(name)
+
+//      Patient.find(patient.id).get.addressGroup must not be(None)
+//      Patient.find(patient.id).get.addressGroup.toOption.get.addresses.toList must not be(None)
+//      Patient.find(patient.id).get.addressGroup.toOption.get.addresses.toList.head must equalTo(address)
 //      Patient.find(patient.id).get.addressGroup.toOption.get.mainAddress.toOption must not be(None)
 //      Patient.find(patient.id).get.addressGroup.toOption.get.mainAddress.toOption.get must equalTo(address)
     }
